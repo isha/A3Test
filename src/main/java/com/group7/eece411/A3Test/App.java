bd370ea6473932c90df347f9bc91ce2d30c37bd3
Isha <isha.kumaarr@gmail.com>
2015-02-17 18:58:50 -0800

    refactor

--- src/main/java/com/group7/eece411/A3Test/App.java
+++ src/main/java/com/group7/eece411/A3Test/App.java
@@ -1,69 +1,47 @@
 package com.group7.eece411.A3Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Vector;
+
+
 
 public class App 
-{
+{	
+	public static final int SUCCESS = 0x00;
+	public static final int NON_EXISTENT_KEY = 0x01;
+	public static final int OUT_OF_SPACE = 0x02;
+	public static final int SYSTEM_OVERLOAD = 0x03;
+	public static final int INTERNAL_FAILURE = 0x04;
+	public static final int UNRECOGNIZED_COMMAND = 0x05;
+	
+	public static final String NO_VALUE = "";
+	
+	public static Tests test;
+	
     public static void main( String[] args ) throws Exception
     {
-    	byte[] response;
-    	
-    	System.out.println("Testing PUT yolo-batman:yolo-batman-value");
-    	response = send((byte) 0x01, "yolo-batman", "yolo-batman-value");
-    	System.out.println("Expected RESP 0x00, Got "+response[0]);
-    	
-    	System.out.println("\n\nTesting GET yolo-batman");
-    	response = send((byte) 0x02, "yolo-batman", "");
-    	System.out.println("Expected RESP 0x00, Got "+response[0]);
-    	System.out.println("Expected VAL yolo-batman-value, Got "+getValue(response));
-    	
-    	System.out.println("\n\nTesting REMOVE yolo-batman");
-    	response = send((byte) 0x03, "yolo-batman", "");
-    	System.out.println("Expected RESP 0x00, Got "+response[0]);
-    	
-    	System.out.println("\n\nTesting REMOVE non-existant-key");
-    	response = send((byte) 0x03, "non-existant-key", "");
-    	System.out.println("Expected RESP 0x01, Got "+response[0]);
-    	
-    	System.out.println("\n\nTesting FAKECOMMAND yolo-batman");
-    	response = send((byte) 0x04, "yolo-batman", "");
-    	System.out.println("Expected RESP 0x05, Got "+response[0]);
+    	test = new Tests("file/hosts.txt");
+
+    	assertThat(SUCCESS, NO_VALUE, test.put("yolo-batman", "yolo-batman-value"));
+    	assertThat(SUCCESS, "yolo-batman-value", test.get("yolo-batman"));
+    	assertThat(SUCCESS, NO_VALUE, test.remove("yolo-batman"));
     }
     
-	private static byte[] send(byte command, String key, String value) throws Exception {
-		// Prep key bytes
-		byte[] keyBytes = new byte[32];
-		keyBytes = key.getBytes("UTF-8");
-		
-		// Prep value bytes
-		byte[] valueBytes = value.getBytes("UTF-8");
-		
-		// Form message with correct sizes for stuff
-		ByteBuffer buffer = ByteBuffer.allocate(1+32+2+valueBytes.length).order(ByteOrder.LITTLE_ENDIAN);
-		buffer.put(command).put(keyBytes).putShort((short) valueBytes.length).put(valueBytes);
-		
-		// Send message
-		UDPClient client = new UDPClient(4567);
-		client.send("127.0.0.1", String.valueOf(7777), new String(buffer.array(), Charset.forName("UTF-8")) );
-		
-		// Receive message
-		client.setTimeout(2000);
-		byte[] rcvMsg = client.receive();
-		
-		byte[] actualMsg = new Header().decodeAndGetMessage(rcvMsg);
-		return actualMsg;
-	}
-
-	private static String getValue(byte[] msg) {
-		ByteBuffer bf = ByteBuffer.wrap(msg).order(ByteOrder.LITTLE_ENDIAN);
-		byte responseCode = bf.get();
-		short valueSize = bf.getShort();
-		byte[] rcvValue = new byte[valueSize];
-		bf.get(rcvValue, 3, valueSize);
-		String value = new String(rcvValue, Charset.forName("UTF-8"));
-		
-		return value;
-	}
+    public static void assertThat(int respCode, String value, byte[] respBytes) {
+    	int rc = test.getResponseCode(respBytes);
+    	String v = test.getValue(respBytes);
+    	
+    	if (respCode == rc && v.equals(value)) {
+    		System.out.println("OK expected response code: "+respCode+" got: "+rc+", expected value: "+value+" got: "+v);
+    	} else {
+    		System.out.println("FAIL expected response code: "+respCode+" got: "+rc+", expected value: "+value+" got: "+v);
+    	}
+    }
 }
---
+++ src/main/java/com/group7/eece411/A3Test/Tests.java
@@ -0,0 +1,101 @@
+package com.group7.eece411.A3Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.Charset;
+import java.util.Vector;
+
+public class Tests {
+	public class NodeInfo {
+		public NodeInfo(String hn, String p) {
+			hostName = hn;
+			port = p;
+		}
+		public String hostName;
+		public String port;
+	}
+	
+	Vector<NodeInfo> nodes = new Vector<NodeInfo>();
+	
+	public byte[] put(String k, String v) throws Exception {
+		byte[] response;
+		System.out.println("\nPUT key: "+k+", value: "+v);
+    	response = send((byte) 0x01, k, v);
+    	return response;
+	}
+	
+	public byte[] get(String k) throws Exception {
+		byte[] response;
+		System.out.println("\nGET key: "+k);
+    	response = send((byte) 0x02, k, "");
+    	return response;
+	}
+	
+	public byte[] remove(String k) throws Exception {
+		byte[] response;
+		System.out.println("\nREMOVE key: "+k);
+    	response = send((byte) 0x03, k, "");
+    	return response;
+	}
+	
+	public Tests(String string) throws IOException {
+		InputStream in = Tests.class.getClass().getResourceAsStream("/"+
+				string);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			String[] lineArray = line.split(":");
+			if (lineArray.length != 2) {
+				System.out.println("Invalid Line Found in hosts.txt");
+			} else {
+				NodeInfo n = new NodeInfo(lineArray[0], lineArray[1]);
+				nodes.add(n);
+				System.out.println("Identified participating node "+n.hostName+" on port "+n.port);
+			}
+		}
+	}
+	
+	private byte[] send(byte command, String key, String value) throws Exception {
+		// Prep key bytes
+		byte[] keyBytes = new byte[32];
+		keyBytes = key.getBytes("UTF-8");
+		
+		// Prep value bytes
+		byte[] valueBytes = value.getBytes("UTF-8");
+		
+		// Form message with correct sizes for stuff
+		ByteBuffer buffer = ByteBuffer.allocate(1+32+2+valueBytes.length).order(ByteOrder.LITTLE_ENDIAN);
+		buffer.put(command).put(keyBytes).putShort((short) valueBytes.length).put(valueBytes);
+		
+		// Send message
+		UDPClient client = new UDPClient(4567);
+		client.send("127.0.0.1", String.valueOf(7777), new String(buffer.array(), Charset.forName("UTF-8")) );
+		
+		// Receive message
+		client.setTimeout(2000);
+		byte[] rcvMsg = client.receive();
+		
+		byte[] actualMsg = new Header().decodeAndGetMessage(rcvMsg);
+		return actualMsg;
+	}
+
+	public String getValue(byte[] msg) {
+		ByteBuffer bf = ByteBuffer.wrap(msg).order(ByteOrder.LITTLE_ENDIAN);
+		byte responseCode = bf.get();
+		short valueSize = bf.getShort();
+		byte[] rcvValue = new byte[valueSize];
+		bf.get(rcvValue, 3, valueSize);
+		String value = new String(rcvValue, Charset.forName("UTF-8"));
+		
+		return value;
+	}
+	
+	public int getResponseCode(byte[] msg) {	
+		return msg[0];
+	}
+
+}
